3873d853c66ff09c1bb3e79eb1f327715c9ceb84
NeonGames <72770312+Starplayer39@users.noreply.github.com>
2022-10-20 17:26:12 +0000

    Update loader.ts

--- src/loader.ts
+++ src/loader.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 
 import { commands_folder_dir } from '../config.json'
 
-function loadCommands(iter: any) {
+function loadCommands(iter: Function) {
     const commandsPath = commands_folder_dir
     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
     
