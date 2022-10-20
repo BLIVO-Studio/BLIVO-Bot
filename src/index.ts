feba13618d547c54ffe30bba907fb20751cac15f
NeonGames <72770312+Starplayer39@users.noreply.github.com>
2022-10-20 08:13:41 +0000

    Set up the project again & Add basic bot code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello TS!")
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { token, load_token_from_env } from '../config.json';
+
+let TOKEN : string | undefined
+
+if (load_token_from_env) {
+    TOKEN = process.env['TOKEN']
+} else {
+    TOKEN = token
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (client) => {
+	console.log(`Ready! Logged in as ${client.user.tag}`);
+});
+
+client.login(TOKEN);
